bb66a9d5052a18f832f3bdd3c89b607d2f411609
unknown <priyanshuprogrammer49@gmail.com>
2024-10-24 11:25:57 +0530

    my 1st test

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div className="bg-black text-white container text-center">
-      This is app helo priyanshu, app kam kar raha hai
+      {/* import all the components */}
     </div>
   );
 };
